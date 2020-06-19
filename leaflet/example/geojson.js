5793460802b2671c239449f673ddd3a59a33097d
vrdeals <65559773+vrdeals@users.noreply.github.com>
2020-06-19 12:18:08 +0200

    Add files via upload

---
+++ leaflet/example/geojson.js
@@ -0,0 +1,97 @@
+;(function (window) {
+  // geoJson definitions for country
+  window.countries = [{
+    type: 'Feature',
+    properties: {
+      name: 'Kreis 1'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [1258, 911]
+    }
+  }, {
+    type: 'Feature',
+    properties: {
+      name: 'Kreis 2'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [1324, 1580]
+    }
+  }, {
+    type: 'Feature',
+    properties: {
+      name: 'Kreis 3'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [1498, 1662]
+    }
+  }, {
+    type: 'Feature',
+    properties: {
+      name: 'Kreis 4'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [1608, 1918]
+    }
+  }, {
+    type: 'Feature',
+    properties: {
+      name: 'Kreis 5'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [1923, 2093]
+    }
+  }, {
+    type: 'Feature',
+    properties: {
+      name: 'Kreis 6'
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: [1374, 2148]
+    }
+  }]
+
+  // geoJson definitions
+  window.geoInfo = [{
+    'type': 'Feature',
+    'properties': {
+      'name': 'Markierung 1'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [1589, 1447]
+    }
+  }, {
+    'type': 'Feature',
+    'properties': {
+      'name': 'Markierung 2'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [2090, 1407]
+    }
+  }, {
+    'type': 'Feature',
+    'properties': {
+      'name': 'Markierung 3'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [2028, 2453]
+    }
+  }, {
+    'type': 'Feature',
+    'properties': {
+      'name': 'Markierung 4'
+    },
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [2623, 1918]
+    }
+  }]
+}(window))
